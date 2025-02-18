9eeccfa130dcbd5d47cdc1ac91b2641df3501781
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-02-18 17:27:08 -0300

    feat(responsive carossel): init add supabase

--- app/(home)/_component/responsive_carossel.tsx
+++ app/(home)/_component/responsive_carossel.tsx
@@ -2,9 +2,11 @@
 import { useState, useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import { createSupabaseClient } from "@/app/_utils/supabase/client";
 
 const ResponsiveCarousel = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [bannerImages, setBannerImages] = useState<string[]>([]);
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
@@ -13,16 +15,25 @@ const ResponsiveCarousel = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const images = [
-    {
-      name: "Banner 1",
-      src: isMobile ? "/banner1-sm.png" : "/banner1-lg.png",
-    },
-    {
-      name: "Banner 2",
-      src: isMobile ? "/banner1-sm.png" : "/banner1-lg.png",
-    },
-  ];
+  useEffect(() => {
+    const supabase = createSupabaseClient();
+
+    const fetchImages = async () => {
+      const { data: imageUrls, error } = await supabase
+        .from("banners")
+        .select("*");
+
+      if (error) {
+        console.error("Erro ao buscar imagens:", error);
+        return;
+      }
+
+      setBannerImages(imageUrls);
+      console.log(imageUrls);
+    };
+
+    fetchImages();
+  }, []);
 
   return (
     <Carousel
@@ -36,12 +47,12 @@ const ResponsiveCarousel = () => {
       emulateTouch
       className="lg:max-w-[1150px] lg:max-h-[240px] sm:max-w-full sm:max-h-[160px] rounded-md overflow-hidden p-0 shadow"
     >
-      {images.map((image, index) => (
+      {bannerImages.map((image, index) => (
         <img
           key={index}
-          src={image.src}
-          alt={image.name}
-          className="w-[1150px]"
+          src={image}
+          alt={`Banner ${index + 1}`}
+          className="w-full object-cover"
         />
       ))}
     </Carousel>
