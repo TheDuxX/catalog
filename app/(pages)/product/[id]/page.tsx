ae9c7b0d1af5879e4891192c2864d2127dc82d93
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-02-19 22:45:36 -0300

    feat: add formattedPrice and tooltip in price

--- app/(pages)/product/[id]/page.tsx
+++ app/(pages)/product/[id]/page.tsx
@@ -7,9 +7,15 @@ import {
 } from "@/app/_components/ui/breadcrumb";
 import { Button } from "@/app/_components/ui/button";
 import { FindUniqueProduct } from "@/app/_lib/utils";
-import { Minus, Plus } from "lucide-react";
+import { MessageCircleQuestionIcon, Minus, Plus } from "lucide-react";
 import ResponsiveCarousel from "./_component/responsive_carossel";
 import SaleButton from "./_component/sale-button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/app/_components/ui/tooltip";
 
 interface ProdctDetailsPageProps {
   params: {
@@ -29,6 +35,11 @@ const ProductDetailPage = async ({ params }: ProdctDetailsPageProps) => {
   if (!product) {
     return <div>Produto não encontrado</div>;
   }
+
+  const formattedPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(product.price);
 
   return (
     <>
@@ -67,18 +78,29 @@ const ProductDetailPage = async ({ params }: ProdctDetailsPageProps) => {
             <div className="w-full h-auto rounded-md">
               <ResponsiveCarousel product={product} />
             </div>
-            <div className="w-full flex flex-col gap-4 ">
-              <div>
-                <h2 className="font-semibold text-4xl">{product.name}</h2>
-                <small>Ref:{product.reference}</small>
-                <p>Marca:{product.mark.name}</p>
-                <p>Categoria:{product.category.name}</p>
-                <p>{product.description}</p>
-              </div>
-              <div className="w-full p-4 flex flex-col gap-4">
-                <h2 className="font-semibold text-2xl">
-                  R$ {product.price}{" "}
-                  <small className="text-sm font-normal">à vista / un.</small>
+            <div className="w-full flex flex-col gap-4 p-4">
+              <h2 className="font-semibold text-4xl">{product.name}</h2>
+              <small>Ref:{product.reference}</small>
+              <p>Marca:{product.mark.name}</p>
+              <p>Categoria:{product.category.name}</p>
+              <p>{product.description}</p>
+              <div className="w-full  flex flex-col gap-4">
+                <h2 className="font-semibold text-4xl">
+                  {formattedPrice}
+                  <small className="text-sm font-normal">
+                    {" "}
+                    à vista / un.{" "}
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger className="">
+                          <MessageCircleQuestionIcon className="fill-primary text-white" size={16}/>
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-white text-black shadow" align="start">
+                          <p>O preço pode variar de acordo com a forma de pagamento ou disponibilidade do produto</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </small>
                 </h2>
                 <div>
                   <p>Selecione a quantidade desejada.</p>
@@ -88,7 +110,7 @@ const ProductDetailPage = async ({ params }: ProdctDetailsPageProps) => {
                     <Minus className="opacity-50" />
                   </div>
                 </div>
-                <SaleButton product={product}/>
+                <SaleButton product={product} />
               </div>
             </div>
           </div>
