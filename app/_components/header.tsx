1aeb16a9b974726adbd46c992cb3e37f73059faa
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-02-21 22:35:49 -0300

    perf: add mobile menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,19 @@
 "use client";
 import { Button } from "@/app/_components/ui/button";
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import path from "path";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import SearchBar from "./search";
 
 interface ProdctDetailsPageProps {
   params: {
@@ -24,8 +34,8 @@ const Header = () => {
 
   return (
     <>
-      <div className="w-full h-auto shadow-md  flex flex-col justify-center items-center">
-        <div className="w-full max-w-[1150px] max-h-24 flex justify-start md:justify-center px-2  items-center z-10 bg-background">
+      <div className="w-full h-auto shadow-md flex md:flex-col flex-row justify-center items-center px-2 bg-background">
+        <div className="w-full max-w-[1150px] max-h-24 flex justify-start md:justify-center px-2  items-center z-10 ">
           <div className="relative md:w-[300px] w-[200px] h-24">
             <Image
               src={"/logo.png"}
@@ -35,6 +45,30 @@ const Header = () => {
             />
           </div>
         </div>
+        <div className="block md:hidden px-2">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <MenuIcon size={36} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="max-w-[200px] mx-4 flex flex-col gap-2">
+              {link.map((link, index) => (
+                <DropdownMenuItem
+                  key={`${link.href}-${index}`}
+                  className={` text-xl w-full flex  justify-end ${
+                    pathname === link.href ? "font-bold text-xl italic " : ""
+                  }`}
+                >
+                  <Link href={link.href}>{link.name}</Link>
+                  {pathname === link.href ? (
+                    <div className="w-1 h-5 bg-secondary rounded-full"></div>
+                  ) : (
+                    ""
+                  )}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
         <div className="w-full min-h-16 h-full md:flex flex-row items-center justify-center bg-secondary hidden">
           <div className="lg:max-w-[1140px] w-full flex flex-row justify-center items-center gap-1">
             {link.map((link, index) => (
@@ -42,9 +76,7 @@ const Header = () => {
                 key={`${link.href}-${index}`} // Garante que a chave é única
                 href={link.href}
                 className={`hover:text-secondary hover:bg-primary w-full h-full p-5 rounded-md text-lg text-center ${
-                  pathname === link.href
-                    ? "font-bold text-xl italic "
-                    : ""
+                  pathname === link.href ? "font-bold text-xl italic " : ""
                 }`}
               >
                 {link.name}
