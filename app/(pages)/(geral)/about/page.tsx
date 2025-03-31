f928ff5ef97f51abf14ea9b6670daac48f62b170
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-03-31 14:08:53 -0300

    feat: add example image on about's page

--- app/(pages)/(geral)/about/page.tsx
+++ app/(pages)/(geral)/about/page.tsx
@@ -1,14 +1,22 @@
+import Image from "next/image";
+
 const About = () => {
   return (
     <div className="w-full flex items-center justify-center my-4 px-4">
       <div className="w-full max-w-full md:max-w-[1150px] grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-2">
-        
         {/* Imagem 1 */}
-        <div className="w-full h-48 md:h-full bg-gray-300 rounded-md order-1 md:order-none"></div>
+        <div className="w-full h-48 md:min-h-[320px] rounded-md order-1 md:order-none relative overflow-hidden shadow-sm">
+          <Image
+            src={"/about-image.png"}
+            alt="about"
+            fill
+            className="object-fill"
+          />
+        </div>
 
         {/* Texto 1 */}
         <div className="w-full order-2 md:order-none">
-          <p className="text-justify indent-10">
+          <p className="text-justify indent-10 p-2">
             Fundada em 2013 por José Casturino dos Santos, a Tratorino – Peças
             para Máquinas e Tratores nasceu com o propósito de oferecer soluções
             eficientes para o setor agrícola, florestal e industrial. Com mais
@@ -19,12 +27,9 @@ const About = () => {
           </p>
         </div>
 
-        {/* Imagem 2 */}
-        <div className="w-full h-48 md:h-full bg-gray-300 rounded-md order-3 md:order-none"></div>
-
         {/* Texto 2 */}
         <div className="w-full order-4 md:order-none">
-          <p className="text-justify indent-10">
+          <p className="text-justify indent-10 p-2">
             Localizada em Telêmaco Borba, Paraná, a Tratorino se destaca pelo
             compromisso com o atendimento personalizado, buscando sempre
             oferecer as melhores opções para manutenção e reposição de
@@ -39,6 +44,15 @@ const About = () => {
           </p>
         </div>
 
+        {/* Imagem 2 */}
+        <div className="w-full h-48 md:min-h-[320px] rounded-md order-3 md:order-none relative overflow-hidden shadow-sm">
+          <Image
+            src={"/about-image.png"}
+            alt="about"
+            fill
+            className="object-fill"
+          />
+        </div>
       </div>
     </div>
   );
