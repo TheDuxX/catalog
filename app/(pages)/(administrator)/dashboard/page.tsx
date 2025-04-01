3d778880938793663bc7cc70291d9e714f0e39ac
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-04-01 16:47:23 -0300

    feat: adjust css

--- app/(pages)/(administrator)/dashboard/page.tsx
+++ app/(pages)/(administrator)/dashboard/page.tsx
@@ -1,12 +1,14 @@
 import MetricCards from "./_components/cards";
+import MetricCharts from "./_components/chart";
 import DashboardHeader from "./_components/dashboard-header";
 
 const DashboardPage = () => {
   return (
     <>
       <DashboardHeader />
-      <div className="p-4">
+      <div className="p-4 flex flex-col gap-4">
         <MetricCards />
+        <MetricCharts />
       </div>
     </>
   );
