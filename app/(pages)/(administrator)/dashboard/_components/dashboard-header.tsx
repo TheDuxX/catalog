1e9bd93ba35f031a9ae64000f7a115782d598866
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-04-07 16:52:59 -0300

    style: clean code

--- app/(pages)/(administrator)/dashboard/_components/dashboard-header.tsx
+++ app/(pages)/(administrator)/dashboard/_components/dashboard-header.tsx
@@ -1,9 +1,8 @@
 "use client";
-import SearchBar from "@/app/_components/search";
-import { Button } from "@/app/_components/ui/button";
-import { ChevronLeft, User } from "lucide-react";
+import { User } from "lucide-react";
 import { useDashboardHeaderViewModel } from "../_viewmodels/useDashboardHeaderViewModel";
 import { SidebarTrigger } from "@/app/_components/ui/sidebar";
+import SearchBar from "./dashboard-search-bar";
 
 const DashboardHeader = () => {
   const { currentTitle, goBack } = useDashboardHeaderViewModel();
@@ -11,7 +10,7 @@ const DashboardHeader = () => {
   return (
     <div className="w-full h-auto bg-white p-2 border-b border-gray-200 flex flex-row justify-between items-center gap-2">
       <div className="flex-row flex items-center gap-2 w-1/4 text-gray-700">
-        <SidebarTrigger className="hover:bg-transparent"/>
+        <SidebarTrigger className="hover:bg-transparent" />
         <h2 className="font-semibold text-2xl">{currentTitle}</h2>
       </div>
       <div className="flex-row flex items-center gap-2 w-2/4">
