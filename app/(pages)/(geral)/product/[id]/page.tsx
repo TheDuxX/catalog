d6da84f8187142b78b914230c4b81b2c7b10bc20
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-05-16 22:39:45 -0300

    params type test

--- app/(pages)/(geral)/product/[id]/page.tsx
+++ app/(pages)/(geral)/product/[id]/page.tsx
@@ -1,12 +1,6 @@
 import ProductDetailService from "./_component/product-details";
 
-interface ProductDetailsPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function ProductDetailPage({ params }: ProductDetailsPageProps) {
+export default function ProductDetailPage({ params }: any) {
   return (
     <div className="w-full flex flex-col justify-center items-center px-2 lg:px-0">
       <ProductDetailService id={params.id} />
