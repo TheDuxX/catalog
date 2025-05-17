f8e05f4a3966055b7db1e04e8dec607e86b2f432
Alex Edurdo <125306500+TheDuxX@users.noreply.github.com>
2025-05-16 23:57:48 -0300

    fix: add suspense

--- app/(pages)/(administrator)/dashboard/page.tsx
+++ app/(pages)/(administrator)/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import MetricCards from "./_components/cards";
 import MetricCharts from "./_components/chart";
 import DashboardHeader from "./_components/dashboard-header";
@@ -6,12 +7,20 @@ import DashboardMostView from "./_components/dashboard-mostview";
 
 const DashboardPage = () => {
   return (
-    <div className="h-full ">
-      <DashboardHeader />
+    <div className="h-full">
+      <Suspense fallback={<div>Carregando cabeçalho...</div>}>
+        <DashboardHeader />
+      </Suspense>
       <div className="p-2 flex flex-col gap-2">
-        <MetricCards />
-        <MetricCharts />
-        <DashboardMostView />
+        <Suspense fallback={<div>Carregando métricas...</div>}>
+          <MetricCards />
+        </Suspense>
+        <Suspense fallback={<div>Carregando gráficos...</div>}>
+          <MetricCharts />
+        </Suspense>
+        <Suspense fallback={<div>Carregando produtos mais vistos...</div>}>
+          <DashboardMostView />
+        </Suspense>
       </div>
     </div>
   );
