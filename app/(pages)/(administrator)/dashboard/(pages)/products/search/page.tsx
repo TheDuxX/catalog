32f66fdbece916be68990a6fa4d142150f8ffe7b
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-05-19 13:55:58 -0300

    fix: add suspense

--- app/(pages)/(administrator)/dashboard/(pages)/products/search/page.tsx
+++ app/(pages)/(administrator)/dashboard/(pages)/products/search/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import SearchResults from "../../../_components/search-results";
 
 const DashboardSearchResults = () => {
   return (
     <>
-      <SearchResults />
+      <Suspense>
+        <SearchResults />
+      </Suspense>
     </>
   );
 };
