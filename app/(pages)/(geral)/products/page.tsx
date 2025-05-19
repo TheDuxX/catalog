eff8c50bebf4f2680e73e49275a7f394e0b964af
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-05-19 14:13:03 -0300

    fix: add suspense

--- app/(pages)/(geral)/products/page.tsx
+++ app/(pages)/(geral)/products/page.tsx
@@ -1,10 +1,13 @@
 import List from "@/app/_components/list";
+import { Suspense } from "react";
 
 const ProductsList = () => {
   return (
     <div className="flex flex-col justify-center items-center py-2">
       <div className="w-full max-w-[1150px]">
-        <List />
+        <Suspense>
+          <List />
+        </Suspense>
       </div>
     </div>
   );
