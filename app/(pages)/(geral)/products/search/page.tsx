b5469f1520b4771bb95be62b43d83dec95738de9
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2025-05-19 13:53:28 -0300

    fiz: add suspense

--- app/(pages)/(geral)/products/search/page.tsx
+++ app/(pages)/(geral)/products/search/page.tsx
@@ -1,12 +1,15 @@
 import SearchResults from "@/app/(pages)/(administrator)/dashboard/_components/search-results";
 import SearchBar from "@/app/_components/search";
+import { Suspense } from "react";
 
 const SearchPage = () => {
   return (
     <div className="flex flex-col justify-center items-center pt-4">
       <div className="w-full max-w-[1150px]">
-        <SearchBar />
-        <SearchResults />
+        <Suspense>
+          <SearchBar />
+          <SearchResults />
+        </Suspense>
       </div>
     </div>
   );
